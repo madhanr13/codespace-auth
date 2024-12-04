081c7aab71bc3a9e7b9437ae47fe506bf7974927
Madhan R <135804250+madhanr13@users.noreply.github.com>
2024-12-04 14:13:59 +0000

    day 1 changes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const app = require("./app.js")
 const {PORT} = process.env
 
-app.get("/", ()=> {
-    res.send("<h1>Server is working</h1>")
+app.get("/", (req, res)=> {
+    res.send("<h1>Server is working successfully</h1>")
 })
 
 app.listen(PORT, ()=>{
